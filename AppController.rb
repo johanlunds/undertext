577d2e5319375806d219c7bec61f4cd3de2541ef
lundan <lundan@2bab485a-edb9-11dd-9974-47326a7b9159>
2009-01-29 02:38:57 +0000

    Recursive folder search for files.

--- AppController.rb
+++ AppController.rb
@@ -8,6 +8,13 @@
 
 class AppController < OSX::NSWindowController
   include OSX
+  
+  # Used if looking by UTI is too cumbersome
+  EXTS = %w(3g2 3gp 3gp2 3gpp 60d ajp asf asx avchd avi bik bix box cam dat divx 
+    dmf dv dvr-ms evo flc fli flic flv flx gvi gvp h264 m1v m2p m2ts m2v m4e m4v
+    mjp mjpeg mjpg mkv moov mov movhd movie movx mp4 mpe mpeg mpg mpv mpv2 mxf
+    nsv nut ogg ogm omf ps qt ram rm rmvb swf ts vfw vid video viv vivo vob vro
+    wm wmv wmx wrap wvx wx x264 xvid)
 
   ib_outlets :outline, :status
   
@@ -31,8 +38,13 @@ class AppController < OSX::NSWindowController
     end
   end
   
-  def application_openFiles(sender, filenames)
-    # add in outline
+  # for folders it searches recursively for movie files
+  def application_openFiles(sender, paths)
+    files, folders = paths.partition { |path| File.file? path }
+    folders.each do |folder|
+      files += Dir.glob(folder + "/**/*.{#{EXTS.join(',')}}")
+    end
+    @outline.dataSource.setFiles(files)
   end
   
   # Can choose directory and/or multiple files (movies)
