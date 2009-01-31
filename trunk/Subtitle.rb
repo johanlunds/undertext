cd4eb6e5281715f56684dc9eb254e4bb0b121d88
johanlunds <johanlunds@2bab485a-edb9-11dd-9974-47326a7b9159>
2009-01-31 04:58:34 +0000

    Checkbox in interface for selecting subs to be downloaded. Checking movie items checks all children too.

--- trunk/Subtitle.rb
+++ trunk/Subtitle.rb
@@ -7,15 +7,25 @@
 #
 
 class Subtitle < NSObject
-  
-  attr_reader :info
+
+  attr_reader :info, :download
   
   # todo: remove movie info (keys starting with "Movie")
   # todo: when need to filter etc by language, create language class
   def initWithInfo(info)
     init
     @info = info
+    @download = NSOffState
     self
+  end
+  
+  def download=(value)
+    @download = (value != NSOffState) ? NSOnState : NSOffState
+  end
+  
+  # boolean value instead of internal NSOnState/NSOffState
+  def download?
+    @download == NSOnState
   end
   
   def title
